d140806d10c3d01e13b141da4a1f0695e9bc2944
Debora <deboravolc2@gmail.com>
2024-09-26 23:11:54 -0300

    iniciar o repositorio

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+
+app.get("/", function(req, res){
+    res.send("Bem-vindo ao Node.js")
+})
+
+app.get("/contato", function(req,res){
+    res.sendFile(__dirname + "/html/index.html")
+})
+
+//recebendo parametros
+app.get("produtos/:item", function(req,res){
+    res.send("Item: " + req.params.item + "<br>Quantidade: " + req.params.quantidade)
+})
+
+//criando o servidor web com a porta 8081
+app.listen(8081, function(){
+    console.log("Servidor Ativo na porta 8081!");
+}) 
+
+
